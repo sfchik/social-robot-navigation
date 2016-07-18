f4b79ba2716a42719b130b998c7cdc39ba28e4eb
Chik Sheng Fei <sfchik@users.noreply.github.com>
2016-07-18 14:58:56 +0800

    social robot navigation source code
    Without any wrappers.

---
+++ human_follow_goal.cpp
@@ -0,0 +1,123 @@
+#include <ros/ros.h>
+#include <pedsim_msgs/TrackedPersons.h>
+#include <simple_navigation_goals/human_follow_goal.h>
+#include <actionlib/client/simple_action_client.h>
+#include <cmath>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+using namespace std;
+
+HumanFollowGoal::HumanFollowGoal(const ros::NodeHandle &node)
+  : nh_(node)
+{
+	init_ = false;
+
+	//setup subscriber
+	sub_people_pos_ = nh_.subscribe("/pedsim/tracked_persons", 1,  &HumanFollowGoal::callbackPeoplePose, this);
+	sub_robot_pos_ = nh_.subscribe("/pedsim/robot_position", 1, &HumanFollowGoal::callbackRobotPose, this);
+
+	//initializing parameters
+	tracked_ = false;
+	ready_ = false;
+
+	init_ = true;
+}
+
+HumanFollowGoal::~HumanFollowGoal()
+{
+
+}
+
+void HumanFollowGoal::runHumanFollow()
+{
+	ros::Rate r(10);
+
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  //wait for the action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+	while (ros::ok()) {
+		ros::spinOnce();
+		r.sleep();
+		
+		if(ready_ == true) {
+  		ac.sendGoal(goal);
+			ready_ = false;
+		}
+	}
+}
+
+void HumanFollowGoal::callbackPeoplePose(const pedsim_msgs::TrackedPersons::ConstPtr& msg)
+{
+	goal.target_pose.header.frame_id = "odom";  //here odom is the global frame
+	goal.target_pose.header.stamp = ros::Time::now();
+
+	geometry_msgs::Quaternion quat;
+	geometry_msgs::PoseStamped msg_convert;
+	double yaw;
+	float diff_x, diff_y, len; 
+
+	if(tracked_ == false) {
+		for(char i = 0; i < msg->tracks.size(); i++) {
+			yaw = tf::getYaw(msg->tracks[i].pose.pose.orientation); //ped heading towards robot goal
+		
+			diff_x = msg->tracks[5].pose.pose.position.x - pos_robot_x;
+			diff_y = msg->tracks[5].pose.pose.position.y - pos_robot_y;
+			len = sqrt(diff_x*diff_x + diff_y*diff_y);
+			float deg = atan(diff_y/diff_x);  //this is the deg bet. line projections to goal and ped
+
+//			ROS_INFO("x=%.2f y=%.2f len=%.2f deg=%.2f", diff_x, diff_y, len, deg);
+			if(diff_x > 0.0) {
+				if(len < 3.0) {
+					if(deg < 0.785 && deg > -0.785) {  //60 deg
+						if(yaw < 1.047 && yaw > -1.047) {  //10 deg
+							goal.target_pose.pose = msg->tracks[i].pose.pose;	
+							tracked_id = 5;
+							tracked_ = true;	
+						}
+					}
+				}
+			}
+		}
+
+	} else {
+
+		diff_x = msg->tracks[tracked_id].pose.pose.position.x - pos_robot_x;
+		if(diff_x > 0.0) {
+			quat = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.0, 3.142);
+			goal.target_pose.pose.position = msg->tracks[tracked_id].pose.pose.position;
+			goal.target_pose.pose.orientation = quat;
+			ready_ = true;	
+		
+		}
+	}
+}
+
+
+void HumanFollowGoal::callbackRobotPose(const nav_msgs::Odometry::ConstPtr& msg)
+{
+	pos_robot_x = msg->pose.pose.position.x;
+	pos_robot_y = msg->pose.pose.position.y;
+}
+
+int main(int argc, char** argv)
+{
+	//initialize resources
+	ros::init(argc, argv, "human_follow_goal");
+	ros::NodeHandle node;
+	HumanFollowGoal hfg(node);
+
+  if (hfg.init_) {
+      ROS_INFO("node initialized, now running ");
+      hfg.runHumanFollow();
+  }
+  else {
+      ROS_WARN("Could not initialize simulation, aborting");
+      return EXIT_FAILURE;
+  }	
+}
